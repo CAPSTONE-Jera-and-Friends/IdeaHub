c88b26bf2259d17dfced11726e769420cf0bc966
crl-charm <karleee0909@gmail.com>
2026-04-28 22:35:59 +0800

    for inventory checking
    inventory checking
    
    Co-Authored-By: ramosrybelle <188817947+raibill@users.noreply.github.com>
    Co-Authored-By: Jeralyn Protacio  <268843339+protaciojeralyn4@users.noreply.github.com>
    Co-Authored-By: Aj Kurt <268874711+anjeqyl321@users.noreply.github.com>

--- IdeaHubV3/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    SECRET_KEY = "ideahub-secret-key"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/ideahub_pos"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SAMESITE = "Lax"
-SESSION_COOKIE_SECURE = False  
-
